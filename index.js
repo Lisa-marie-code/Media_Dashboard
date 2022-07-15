b0fb94b54394e60756038ac9fb0f52f0f632d10f
Lisa-marie-code <lisamariekoomson340@gmail.com>
2022-07-15 19:25:06 +0000

    dark theme to local storage

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const darkMode = document.getElementById("checkbox");
 const body = document.querySelector("body");
 
 
+const currentTheme = localStorage.getItem("theme");
+if(currentTheme == "dark"){
+    document.body.classList.add("dark-theme");
+}
 
 function toggleTheme(){
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("dark-theme");
+
+    let theme = "light";
+if(document.body.classList.contains("dark-theme")){
+    theme = "dark";
 }
+
+localStorage.setItem("theme",theme);
+}
+
+
+
